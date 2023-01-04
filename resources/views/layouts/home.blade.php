e7bafe829a30e044c516273aa12357791205708c
Steve <steve.greiner@digilife.fr>
2023-01-04 09:48:53 +0400

    mis en page footer

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,25 +10,30 @@
     <link rel="icon" type="image/x-icon" href="/Images/faviconLoto.ico">
 </head>
 <body>
-    @include('layouts.navbar')
-    <div class="mx-5 my-2">
-        @if (session('success'))
-            <p class="alert alert-success mt-3">{{ session('success') }}</p>
-        @endif
-        @if (session('error'))
-            <p class="alert alert-danger mt-3">{{ session('error') }}</p>
-        @endif
+    <div class="d-flex flex-column">
+        @include('layouts.navbar')
+        <div class="mx-5 my-2">
+            @if (session('success'))
+                <p class="alert alert-success mt-3">{{ session('success') }}</p>
+            @endif
+            @if (session('error'))
+                <p class="alert alert-danger mt-3">{{ session('error') }}</p>
+            @endif
+            @include('modals.addParticipant')
+            @include('modals.jouer')
+            @include('modals.addGroup')
+            @include('modals.participantGroup')
+            @include('modals.login')
+            @include('modals.addGain')
+            <div style="min-height: calc(75vh - 80px);">
+               @yield('content') 
+            </div>
+        </div>
+        @include('layouts.footer')
+        </div>
     </div>
-    @include('modals.addParticipant')
-    @include('modals.jouer')
-    @include('modals.addGroup')
-    @include('modals.participantGroup')
-    @include('modals.login')
-    @include('modals.addGain')
     
-    @yield('content')
     
-    @include('layouts.footer')
     <script src="/js/addGain.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
